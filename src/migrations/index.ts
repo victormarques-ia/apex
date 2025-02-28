f950e8b57866e70839fb1523ba6f87d71409dade
Tiago <thrpg@cin.ufpe.br>
2025-02-28 15:05:31 -0300

    feat: add nutritionists

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20250202_232948_initial from './20250202_232948_initial';
 import * as migration_20250224_134819_add_role_and_name_field_to_user from './20250224_134819_add_role_and_name_field_to_user';
 import * as migration_20250228_170337_name_add_trainers from './20250228_170337_name_add_trainers';
 import * as migration_20250228_171018_name_add_agencies from './20250228_171018_name_add_agencies';
+import * as migration_20250228_180503_name_add_nutritionists from './20250228_180503_name_add_nutritionists';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20250228_171018_name_add_agencies.up,
     down: migration_20250228_171018_name_add_agencies.down,
-    name: '20250228_171018_name_add_agencies'
+    name: '20250228_171018_name_add_agencies',
+  },
+  {
+    up: migration_20250228_180503_name_add_nutritionists.up,
+    down: migration_20250228_180503_name_add_nutritionists.down,
+    name: '20250228_180503_name_add_nutritionists'
   },
 ];

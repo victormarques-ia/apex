fd214f75b34676d084038a082d4552a2721dec6e
Tiago <thrpg@cin.ufpe.br>
2025-04-06 17:57:50 -0300

    feat: add notifications

--- src/components/ui/notifications.tsx
+++ src/components/ui/notifications.tsx
@@ -12,12 +12,14 @@ export default function Notifications({ onClose }: NotificationsProps) {
     <div className="fixed top-16 right-4 w-72 max-h-96 bg-white border border-gray-200 shadow-lg rounded-lg p-4 z-20">
       <div className="flex justify-between items-center mb-4">
         <h3 className="font-medium">Notificações</h3>
-        <button className="text-gray-500 hover:text-gray-700" onClick={onClose}>
+        <button className="text-gr</div>ay-500 hover:text-gray-700" onClick={onClose}>
           <X size={18} />
         </button>
       </div>
       <div className="flex flex-col items-center justify-center h-48 bg-gray-50 rounded-lg">
-        <p className="text-gray-500 text-sm">Nenhuma notificação no momento</p>
+        <p className="text-gray-500 text-sm">05/04/25 - Bianca não anotou suas refeições</p>
+        <p className="text-gray-500 text-sm">02/04/25 - Bianca atingiu 100% das metas nutricionais do dia</p>
+        <p className="text-gray-500 text-sm">01/04/25 - Bianca relatou dor muscular após o treino</p>
       </div>
     </div>
   )
